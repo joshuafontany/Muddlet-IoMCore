1abb12299dce6d75982dba360bd7424d4cd16e73
Joshua Fontany <joshua.fontany@gmail.com>
2020-08-13 18:39:53 -0700

    muddler setup

---
+++ IoMCore/build/tmp/config.lua
@@ -0,0 +1,1 @@
+mpackage = "IoMCore-0.1"
---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# You need to use the full path here?
+export PATH="/c/mudlet/demonnic/muddler/bin/:$PATH"
+
+cd /c/iom/Mudlet-IoMCore/IoMCore
+
+muddle
